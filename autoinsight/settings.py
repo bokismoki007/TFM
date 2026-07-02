1d3c0402655eb190c532a334a061bc54c9daf649
bomarbo <bomarbo@alumni.uv.es>
2026-07-02 23:04:47 +0200

    applied deployment setup

--- autoinsight/settings.py
+++ autoinsight/settings.py
@@ -122,8 +122,10 @@ LOGOUT_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join('/tmp', 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 LOGGING = {
     'version': 1,
